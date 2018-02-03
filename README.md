8c7ac40eea03acd53d88207f41e03b080b24b066
Dave Pullig <dave@deliciousmedia.co.uk>
2018-02-03 14:01:44 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# DM Environment Indicator
+
+Add a colour coded indicator to the WordPress admin bar to indicate which environment we're in (local development/staging/live).
+
+It is designed to be used with our [WordPress layout](https://github.com/DeliciousMedia/DM-WordPress-Layout) and uses the `DM_ENVIRONMENT` constant to determine the environment.
+
+## Installation
+
+Install via Composer (`composer require deliciousmedia/dm-ei`), or just clone/copy the file to your mu-plugins folder.
+
+## Usage
+
+---
+Built by the team at [Delicious Media](https://www.deliciousmedia.co.uk/), a specialist WordPress development agency based in Sheffield, UK.
---
+++ dm-ei.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Plugin Name: DM Environment Indicator
+ * Plugin URI: https://github.com/DeliciousMedia/DM-EI
+ * Description: Add a colour coded indicator to the admin bar to indicate which environment we're in.
+ * Version: 1.0.0
+ * Author: Delicious Media Limited
+ * Author URI: https://www.deliciousmedia.co.uk
+ * Text Domain: dm-ei
+ * License: GPLv3 or later
+ * Contributors: davepullig
+ *
+ * @package dm-twilio
+ **/
+
+// Default to unknown if no environment is set.
+defined( 'DM_ENVIRONMENT' ) || define( 'DM_ENVIRONMENT', 'UNKNOWN' );
+
+/**
+ * Add our indicator to the admin bar.
+ *
+ * @param  object $wp_admin_bar WordPress Admin Bar object.
+ * @return void
+ */
+function dmei_add_adminbar_node( $wp_admin_bar ) {
+	$wp_admin_bar->add_node(
+		[
+			'title'  => esc_html( strtoupper( DM_ENVIRONMENT ) ),
+			'id'     => 'dmei',
+			'parent' => 'top-secondary',
+			'meta'   => [
+				'class' => 'dmei',
+			],
+		]
+	);
+}
+add_action( 'admin_bar_menu', 'dmei_add_adminbar_node', 1 );
+
+/**
+ * Insert inline CSS to style our indicator.
+ *
+ * @return void
+ */
+function dmei_insert_css() {
+
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
+
+	switch ( strtoupper( DM_ENVIRONMENT ) ) {
+
+		case 'DEV':
+		case 'LOCAL':
+			$colour = '#00cc00';
+			break;
+
+		case 'STAGING':
+		case 'STAGE':
+		case 'TEST':
+		case 'UAT':
+		case 'PREPROD':
+			$colour = '#ff9933';
+			break;
+
+		case 'LIVE':
+		case 'PROD':
+		case 'PRODUCTION':
+			$colour = '#ff0000';
+			break;
+
+		default:
+			$colour = '#666';
+			break;
+	}
+	?>
+	<style type="text/css">
+		#wpadminbar .dmei .ab-item {
+			background-color: <?php echo esc_html( $colour ); ?> !important;
+			color: #fff !important;
+		}
+	</style>
+	<?php
+}
+add_action( 'wp_head', 'dmei_insert_css' );
+add_action( 'admin_head', 'dmei_insert_css' );
+
